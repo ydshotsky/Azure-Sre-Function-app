05d226fe9c558628f28b2308ebf54fb45be4632f
Harshit Khanna <harshitkhanna006@gmail.com>
2026-05-30 20:02:29 +0530

    added test queue

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import logging
 import azure.functions as func
 from AzureFunctionDeployHttpTrigger.__init__ import http_trigger_bp
 from AzureStorageQueueTriggerFunction.__init__ import queue_trigger_bp
+from AzureStorageQueueTriggerFunction.__init__ import test_bp
 
 # Global Host Engine Logging Adjustments
 logging.basicConfig(level=logging.INFO)
@@ -12,6 +13,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 # Clean, Explicit Subfolder Blueprint Registrations
 app.register_blueprint(http_trigger_bp)
 app.register_blueprint(queue_trigger_bp)
+app.register_blueprint(test_bp)
 
 
 @app.route(route="test")
