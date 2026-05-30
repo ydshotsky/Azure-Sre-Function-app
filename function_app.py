4f06131353194c6262f90aa1fcf671a5ca94c52d
harshit khanna <168678927+ydshotsky@users.noreply.github.com>
2026-05-30 14:29:07 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -12,3 +12,8 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 # Clean, Explicit Subfolder Blueprint Registrations
 app.register_blueprint(http_trigger_bp)
 app.register_blueprint(queue_trigger_bp)
+
+
+@app.route(route="test")
+def test(req: func.HttpRequest):
+    return func.HttpResponse("OK")
